b2d1a147916397c000d37cfa87fc90c4a3fe5bba
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-02-11 16:01:20 -0500

    send html file through GET request

--- myApp.js
+++ myApp.js
@@ -2,38 +2,9 @@ var express = require('express');
 var app = express();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
+console.log("hello world");
+app.get("/", (req, res) => {
+    //res.send("Hello Express")
+    res.sendFile(__dirname + "public/index.html")
+});
