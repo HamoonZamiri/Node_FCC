5cf65929d9bd66fa84205ae2df66d1e72ed728a9
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-03-08 11:42:39 -0500

    json challenge complete

--- myApp.js
+++ myApp.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
-
+const path = __dirname + "/public";
 
 module.exports = app;
-console.log("hello world");
+app.get("/json", (req, res) => {
+  const obj = {"message": "Hello json"}
+  res.json(obj)
+})
 app.get("/", (req, res) => {
     //res.send("Hello Express")
     res.sendFile(__dirname + "/views/index.html")
 });
+app.use("/public", express.static(path));
